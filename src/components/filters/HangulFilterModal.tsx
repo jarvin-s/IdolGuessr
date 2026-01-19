e01c917ffbc927247763bac240bb863a044d24a6
Jarvinnn <Jarvin.Siegers@Student.GildeOpleidingen.nl>
2026-01-19 23:28:05 +0100

    style: update button colors in modals to pink

--- src/components/filters/HangulFilterModal.tsx
+++ src/components/filters/HangulFilterModal.tsx
@@ -112,7 +112,7 @@ export default function HangulFilterModal({
                 <div className='mt-4 flex justify-center'>
                     <button
                         onClick={handleConfirm}
-                        className='w-full cursor-pointer rounded-full bg-[#6521c7] px-4 py-1.5 text-white transition-all hover:bg-[#6521c7]/80 md:w-[150px]'
+                        className='w-full cursor-pointer rounded-full bg-pink-500 px-4 py-1.5 text-white transition-all hover:bg-pink-600 md:w-[150px]'
                     >
                         Start
                     </button>
--- src/components/modals/HangulStartModal.tsx
+++ src/components/modals/HangulStartModal.tsx
@@ -83,7 +83,7 @@ export default function HangulStartModal({
                 <div className='mt-4 flex justify-center'>
                     <button
                         onClick={handleStart}
-                        className='w-[200px] cursor-pointer rounded-full bg-[#6521c7] px-4 py-1.5 text-white transition-all hover:bg-[#6521c7]/80'
+                        className='w-[200px] cursor-pointer rounded-full bg-pink-500 px-4 py-1.5 text-white transition-all hover:bg-pink-600'
                     >
                         Start
                     </button>
